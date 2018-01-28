0badc4cd0b1053a0bc24d7a7bb6bb89f8636aa42
Mark Kelvin Libario <mkelvin.libario@gmail.com>
2018-01-28 18:24:49 -0500

    Dice Object for Dice Games

---
+++ Dice Object/dice.py
@@ -0,0 +1,53 @@
+import random
+
+class Die:
+    def __init__(self, sides=2, value=0):
+        if not sides >= 2:
+            raise ValueError("Must have at least 2 sides")
+
+        if not isinstance(sides, int):
+            raise ValueError("Sides must be a whole number")
+
+        if not isinstance(value, int):
+            raise ValueError("Value must be a whole number")
+
+        self.value = value or random.randint(1, sides)
+
+    #Turn an instance of a die as an integer and give it the ability to do comparisons and add values.
+    def __int__(self):
+        return self.value
+
+    def __eq__(self, other):
+        return int(self) == other
+
+    def __ne__(self, other):
+        return int(self) != other
+
+    def __lt__(self, other):
+        return int(self) < other
+
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+
+    def __le__(self, other):
+        return int(self) < other or int(self) == other
+
+    def __gt__(self, other):
+        return int(self) > other
+
+    def __add__(self, other):
+        return int(self) + other
+
+    def __radd__(self, other):
+        return int(self) + other
+
+    def __repr__(self):
+        return str(self.value)
+
+
+#Create a 6-sided die
+class D6(Die):
+    def __init__(self, value=0):
+        super().__init__(sides=6, value=value)
+
+
